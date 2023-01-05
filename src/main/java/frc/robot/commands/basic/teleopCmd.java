05fdbf1371d6f2b8f947fc9c0ae31dfe43cdd286
Dominic Couture <45298797+mastermind-mayhem@users.noreply.github.com>
2023-01-05 13:05:44 -0500

    Update teleopCmd.java

--- src/main/java/frc/robot/commands/basic/teleopCmd.java
+++ src/main/java/frc/robot/commands/basic/teleopCmd.java
@@ -42,18 +42,20 @@ public class TeleopCmd extends CommandBase {
   // stick.getRawAxis(Constants.DRIVER_SPEED)
   @Override
   public void execute() {
-    // import the controllers, its two separate ones to accomodate for the helicopter joysticks that are split between two
+    // import the controllers, its two separate ones to accomodate for the helicopter joysticks that
+    // are split between two
     turns = driver1.getRawAxis(Controllers.DRIVER_TURN);
     speeds = driver2.getRawAxis(Controllers.DRIVER_SPEED);
     if (Controllers.arcadedriver == true) {
-    // Follow standard order to use arcade driving preset 
-    // Get the axises and apply speed limits 
+      // Follow standard order to use arcade driving preset
+      // Get the axises and apply speed limits
       double turn = slrForTurn.calculate(turns * Constants.SPEEDLIMIT_TURN * -1.0);
       double speed = slrForDrive.calculate(speeds * Constants.SPEEDLIMIT_SPEED);
       drivesubsystem.teleop(speed, turn);
     } else {
       // Follow tank drive preset
-      // turn the retrieved axises in to left and right sides with only the straightline speed limit applied
+      // turn the retrieved axises in to left and right sides with only the straightline speed limit
+      // applied
       double right = slrForTurn.calculate(turns * Constants.SPEEDLIMIT_SPEED);
       double left = slrForDrive.calculate(speeds * Constants.SPEEDLIMIT_SPEED);
       drivesubsystem.teleop(left, right);
