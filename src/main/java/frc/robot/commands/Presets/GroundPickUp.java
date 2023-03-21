2e6e4d8f4237ac6274a62824633401ebd8a98e0f
Dominic Couture <mastermind.mayhem@gmail.com>
2023-03-21 09:57:41 -0400

    Folder Creation

--- src/main/java/frc/robot/commands/Presets/GroundPickUp.java
+++ src/main/java/frc/robot/commands/Presets/GroundPickUp.java
@@ -2,9 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.OneBar;
+package frc.robot.commands.Presets;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.OneBar.PID;
 import frc.robot.commands.Wrist.*;
 import frc.robot.subsystems.OnebarSubsystem;
 import frc.robot.subsystems.WristSubsystem;
@@ -12,11 +13,11 @@ import frc.robot.subsystems.WristSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class BalanceMode extends SequentialCommandGroup {
-  /** Creates a new ArmStraightUp. */
-  public BalanceMode(OnebarSubsystem onebar, WristSubsystem wrists) {
+public class GroundPickUp extends SequentialCommandGroup {
+  /** Creates a new GroundPickUp. */
+  public GroundPickUp(OnebarSubsystem onebar, WristSubsystem wrist) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new PID(onebar, 0), new AutoAlignCmd(wrists, 0));
+    addCommands(new AutoAlignCmd(wrist, 0.5), new PID(onebar, 110));
   }
 }
